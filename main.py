9dd5a222942b9bae90a450de3ac9e18377d8a6fd
AamAchari <alphaxyz35@gmail.com>
2025-05-04 19:28:17 -0400

    Completed L2: Arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_num_characters
 from stats import get_sorted_dict
+import sys
 
 def get_book_text(file_path):
 	with open(file_path) as file:
@@ -8,22 +9,29 @@ def get_book_text(file_path):
 		return file_contents
 
 def main():
-	file_path = "./books/frankenstein.txt"
-	file_contents = get_book_text(file_path)
-	num_words = get_num_words(file_contents.split())
-	char_dict = get_num_characters(file_contents)
-	for key, value in char_dict.items():
-		print(f"'{key}': {value}")
-	sorted_list = get_sorted_dict(char_dict)
-	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
-	print("----------- Word Count ----------")
-	print(f'Found {num_words} total words ')
-	for l in sorted_list:
-		key, value = l.items()
-		if not key[1].isalpha():
-			continue
-		print(f'{key[1]}: {value[1]}')
+	try:
+		if len(sys.argv) != 2:
+			sys.exit(1)
+		file_path = sys.argv[1]
+		file_contents = get_book_text(file_path)
+		num_words = get_num_words(file_contents.split())
+		char_dict = get_num_characters(file_contents)
+		for key, value in char_dict.items():
+			print(f"'{key}': {value}")
+		sorted_list = get_sorted_dict(char_dict)
+		print("============ BOOKBOT ============")
+		print(f'Analyzing book found at {file_path}...')
+		print("----------- Word Count ----------")
+		print(f'Found {num_words} total words ')
+		for l in sorted_list:
+			key, value = l.items()
+			if not key[1].isalpha():
+				continue
+			print(f'{key[1]}: {value[1]}')
+	except SystemExit as e:
+		# print(e.code)
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
 
 if __name__ == '__main__':
 	main()
